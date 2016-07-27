9ddc5716a845ec311b60d9088973f3392181b2e8
Alex Lach <alexglach@gmail.com>
2016-07-27 13:34:19 -0400

    done with CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,6 +10,40 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+
+  end
+
+  def create
+    @task = Task.new(whitelisted_params)
+    @task.save
+    flash[:success] = "Your task has been saved!"
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    flash[:success] = "#{@task.name} "
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(whitelisted_params)
+    redirect_to task_path(@task.id)
+  end
+
+
+  private
+
+  def whitelisted_params
+    params.require(:task).permit(:name, :description, :date)
   end
 
 end

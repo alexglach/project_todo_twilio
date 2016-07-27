3358a98319f69724787f317bace5641acf63a589
Alex Lach <alexglach@gmail.com>
2016-07-27 18:07:17 -0400

    figuring out priority

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727175259) do
+ActiveRecord::Schema.define(version: 20160727214445) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160727175259) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "complete",    default: false
+    t.integer  "priority"
   end
 
 end
